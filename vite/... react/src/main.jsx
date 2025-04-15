4b31ced57b5425b27d9f7f3004e9626357bc75c8
Riddhi <riddhisaraf8@gmail.com>
2025-04-15 08:51:36 +0530

    0904

--- vite/... react/src/main.jsx
+++ vite/... react/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App4 from './App4.jsx'
+import App4 from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App4 />
+    <App />
   </StrictMode>,
 )
